96070dd02ce4b283cf073863bac298050fc3366b
Simon Weber <simon@simonmweber.com>
2013-02-13 01:02:58 -0500

    define grepo

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 """This file contains the schema for the abandoned elaborated-repo sqlite
 database. It is still used when choosing a new random sample."""
 
+import collections
+
 from base64 import b64encode, b64decode
 import cPickle as pickle
 
@@ -9,6 +11,75 @@ from peewee import BooleanField, DateTimeField, IntegerField, TextField
 
 erepo_db = SqliteDatabase('erepo.db', threadlocals=True)
 erepo_db.connect()
+
+
+class YMD(collections.namedtuple('YMD', 'year month day')):
+    """Same purpose as datetime.date, but small and serializable."""
+    __slots__ = ()
+
+    @staticmethod
+    def from_date(date):
+        """Factory from datetime.date or datetime.datetime."""
+        return YMD(date.year, date.month, date.day)
+
+
+class GRepo(collections.namedtuple('GRepo',
+                                   ['name',  # in 'user/repo' format
+                                    'stars',
+                                    'fetch_ymd',  # YMD
+                                    # these are all GitHub apiv3 names:
+                                    'clone_url',
+                                    'created_at',
+                                    'description',
+                                    'fork',
+                                    'forks',
+                                    'git_url',
+                                    'has_downloads',
+                                    'has_issues',
+                                    'has_wiki',
+                                    'homepage',
+                                    'html_url',
+                                    'id',
+                                    'language',
+                                    'master_branch',
+                                    'open_issues',
+                                    'private',
+                                    'pushed_at',
+                                    'size',
+                                    'ssh_url',
+                                    'svn_url',
+                                    'updated_at',
+                                    'url',
+                                    ])
+            ):
+    """A _G_itHub _r_epo stores a snapshot of GitHub repo metadata retrieved from
+    http://developer.github.com/v3/repos/#get on some date."""
+
+    __slots__ = ()
+
+    def __str__(self):
+        return self.name.encode('utf-8')
+
+    def username(self):
+        self.name.split('/')[0]
+
+    def reponame(self):
+        self.name.split('/')[1]
+
+    @staticmethod
+    def from_erepo(erepo):
+        grepo_to_erepo = {'name': '_user_repo',
+                          'stars': '_stars'}
+
+        grepo_to_erepo.update({f: f for f in erepo.__dict__['_data']
+                               if f not in ('_user_repo', '_stars', '_elaborated_at',
+                                            '_elaborated', '_error', '_features',
+                                            '_flagged')})
+
+        kwargs = {g_f: getattr(erepo, e_f) for (g_f, e_f) in grepo_to_erepo.items()}
+        kwargs['fetch_ymd'] = YMD.from_date(erepo._elaborated_at)
+
+        return GRepo(**kwargs)
 
 
 class ERepoModel(Model):

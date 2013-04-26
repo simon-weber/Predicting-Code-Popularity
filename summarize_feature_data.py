9ee6abf81e83dc6e9d7b6f3da95981b62626bf9b
Simon Weber <simon@simonmweber.com>
2013-04-26 18:15:38 -0400

    investigating import relative occurence

--- summarize_feature_data.py
+++ summarize_feature_data.py
@@ -4,8 +4,7 @@ from collections import defaultdict, Counter
 
 import numpy as np
 
-from classes import classify
-from features import all_features
+from classes import classes
 from models import Repo
 
 
@@ -30,7 +29,7 @@ def summarize_imports(class_map):
     print
 
     if len(class_map) == 2:
-        print 'biggest differences'
+        print 'largest absolute differences'
         a_name, b_name = counter_map.keys()
         print "(left is %s)" % a_name
         print
@@ -47,39 +46,69 @@ def summarize_imports(class_map):
             print "    {:.2g} {:.2g}".format(
                 counter_map[a_name][mod], counter_map[b_name][mod])
 
+    print
 
-# def process(repos):
-#     feature_data = {}  # eg 'ReadmeSize' -> np.array(results)
-#
-#     for feature in features:
-#         f_data = []
-#         for repo in repos:
-#             d = f_dicts.get(repo)
-#             if d:
-#                 f_data.append(d[feature])
-#
-#         feature_data[feature] = np.array(f_data)
-#
-#     for feature, ar in feature_data.items():
-#         print '\t'.join(str(x) for x in (
-#             feature,
-#             np.amin(ar),
-#             np.amax(ar),
-#             np.median(ar),
-#             np.mean(ar),
-#             np.std(ar)
-#         ))
+    if len(class_map) == 2:
+        print 'relative differences'
+        print '(only modules in both classes are included)'
+        a_name, b_name = counter_map.keys()
+        a_cnt, b_cnt = counter_map[a_name], counter_map[b_name]
+        print "(left is %s)" % a_name
+        print
+
+        deltas = []  # (import, factor, more_frequent_class)
+        for mod in (counter_map[a_name].viewkeys() &
+                    counter_map[b_name].viewkeys()):
+            # ugh
+            vals = zip((a_cnt[mod], b_cnt[mod]), (a_name, b_name))
+            big_val, big_class_name = max(vals, key=lambda p: p[0])
+            small_val, small_class_name = min(vals, key=lambda p: p[0])
+
+            deltas.append((mod, 1.0 * small_val / big_val, big_class_name))
+
+        deltas.sort(key=lambda t: t[1])
+
+        def output(deltas):
+            for mod, rel_delta, big_class_name in deltas:
+                print " {} {:.2g}".format(mod, rel_delta)
+                print "    {:.2g} {:.2g}".format(
+                    counter_map[a_name][mod], counter_map[b_name][mod])
+
+        print 'smallest'
+        output(deltas[-20:])
+        print
+
+        print 'largest'
+        output(deltas[:20])
+
+
+def summarize_features(class_map, feature_names, class_names,
+                       funcs=[np.amin, np.amax, np.median, np.mean, np.std]):
+    feature_summaries = {}  # eg 'ReadmeSize' -> np.array(num_classes, num_funcs)
+
+    for feature_name in feature_names:
+        class_summaries = []
+        for class_name in class_names:
+            repos = class_map[class_name]
+            feature_data = [getattr(repo, feature_name) for repo in repos]
+
+            class_summaries.append([f(feature_data) for f in funcs])
+
+        feature_summaries[feature_name] = np.array(class_summaries)
+
+    for feature_name, summary in feature_summaries.items():
+        print feature_name
+        for i, class_name in enumerate(class_names):
+            print '  ', class_name
+            print '    ', '\t'.join(str(e) for e in summary[i])
+
+        print '-----'
+        print
 
 
 if __name__ == '__main__':
-    class_map = defaultdict(list)
-    for r in Repo.load_sample():
-        class_map[classify(r)].append(r)
+    class_map = Repo.load_sample(separate=True)
+
+    #summarize_features(class_map, ['readme_size'], sorted(classes))
 
     summarize_imports(class_map)
-
-    # for cls in classes:
-    #     print
-    #     print "=====>", cls
-    #     print
-    #     process(classes[cls])
